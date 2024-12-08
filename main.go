ebe4082eba6a864637ab06d646fb4716b059390d
Larissa <37162993+lariskovski@users.noreply.github.com>
2024-12-08 13:26:41 +0000

    Add user signup and login functionality with HTML templates

--- main.go
+++ main.go
@@ -1,28 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
-)
 
-type hotdog int
+	"github.com/satori/go.uuid"
+)
 
 var tpl *template.Template
 
-type RequestData struct {
-	Method string
-	URL    string
-	Proto  string
-	Header http.Header
-	Form   map[string][]string
-}
-
 func init() {
-	tpl = template.Must(template.ParseFiles("index.gohtml"))
+	tpl = template.Must(template.ParseGlob("templates/*"))
 }
 
-func get(w http.ResponseWriter, r *http.Request) {
+type User struct {
+	Username  string
+	Email string
+	Password string
+}
+
+var dbUsers = map[string]User{} // user ID, user
+
+func signup(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 	if err != nil {
 		// Handle error
@@ -31,21 +32,69 @@ func get(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/html")
 
-	data := RequestData{
-		Method: r.Method,
-		URL:    r.URL.String(),
-		Proto:  r.Proto,
-		Header: r.Header,
-		Form:   r.Form,
+	data := User{
+		Username: r.FormValue("username"),
+		Email: r.FormValue("email"),
+		Password: r.FormValue("password"),
 	}
 
-	err = tpl.Execute(w, data)
+	// Store the user in the database
+	dbUsers[data.Username] = data
+
+	err = tpl.ExecuteTemplate(w, "signup.gohtml", data)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		// Handle error
+		log.Fatal(err)
+	}
+
+	w.Header().Set("Content-Type", "text/html")
+
+	if r.Method == http.MethodPost {
+		data := User{
+			Username: r.FormValue("username"),
+			Email: r.FormValue("email"),
+			Password: r.FormValue("password"),
+		}
+	
+		if u, ok := dbUsers[data.Username]; ok {
+			if u.Password == data.Password {
+				fmt.Fprintln(w, "Welcome back, ", data.Username)
+				_, err := r.Cookie("session")
+				if err == http.ErrNoCookie {
+					id := uuid.NewV4()
+					// If no cookie is found, set a new cookie with value 0
+					http.SetCookie(w, &http.Cookie{
+						Name: "session",
+						Value: id.String(),
+						HttpOnly: true,
+					})
+				} else if err != nil {
+					log.Fatal(err)
+				}
+			} else {
+				fmt.Fprintln(w, "Invalid password")
+	
+			}
+		} else {
+			fmt.Fprintln(w, "User not found")
+		}
+	}
+	err = tpl.ExecuteTemplate(w, "login.gohtml", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
 func main() {
-	http.HandleFunc("/", get)
+	// http.HandleFunc("/", session)
+	http.HandleFunc("/", signup)
+	http.HandleFunc("/login", login)
 	http.ListenAndServe(":8080", nil)
 }
