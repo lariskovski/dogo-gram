bfa1fc8d8a7558545857fa5bfca04c0443e0d379
Larissa <37162993+lariskovski@users.noreply.github.com>
2024-12-09 18:00:21 +0000

    Refactor user handling and session management in main.go for improved clarity and functionality

--- main.go
+++ main.go
@@ -9,6 +9,12 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+type User struct {
+	Username string
+	Email    string
+	Password string
+}
+
 var tpl *template.Template
 var log = logrus.New()
 
@@ -18,50 +24,39 @@ func init() {
 	log.Level = logrus.InfoLevel
 }
 
-type User struct {
-	Username string
-	Email    string
-	Password string
-}
-
-var dbUsers = map[string]User{}       // user ID, user
-var dbSessions = map[string]string{}  // session ID, user ID
-
+var dbUsers = map[string]User{}      // user ID, user
+var dbSessions = map[string]string{} // session ID, user ID
 
 func main() {
 	u1 := User{"teste", "", "teste"}
 	dbUsers["teste"] = u1
 
-	http.HandleFunc("/", bar)
+	http.HandleFunc("/", index)
 	http.HandleFunc("/login", login)
 	http.HandleFunc("/signup", signup)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func bar(w http.ResponseWriter, r *http.Request) {
-	c, err := r.Cookie("session")
+func index(w http.ResponseWriter, r *http.Request) {
+	// If user is not logged in, redirect to login
+	user, err := alreadyLoggedIn(r)
 	if err != nil {
 		http.Redirect(w, r, "/login", http.StatusSeeOther)
 		return
 	}
-
-	username, ok := dbSessions[c.Value]
-	if !ok {
-		http.Redirect(w, r, "/login", http.StatusSeeOther)
-		return
-	}
-
-	user := dbUsers[username]
-	log.WithField("username", user.Username).Info("User accessed bar")
-	fmt.Fprintln(w, "Welcome back,", user.Username)
+	// If user is logged in, render index page
+	log.WithField("username", user.Username).Info("User accessed index")
+	fmt.Fprintln(w, "Welcome back, ", user.Username)
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-	if alreadyLoggedIn(r) {
+	// If user is already logged in, redirect to index
+	if _, err := alreadyLoggedIn(r); err == nil {
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
-	
+
+	// If request is not POST, render login page
 	if r.Method != http.MethodPost {
 		err := tpl.ExecuteTemplate(w, "login.gohtml", nil)
 		if err != nil {
@@ -71,99 +66,123 @@ func login(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err := r.ParseForm()
+	// If request is POST, parse form and authenticate user
+	formUser, err := parseForm(r)
 	if err != nil {
-		log.WithError(err).Error("Unable to parse form")
 		http.Error(w, "Unable to parse form", http.StatusBadRequest)
 		return
 	}
 
-	data := User{
-		Username: r.FormValue("username"),
-		Email:    r.FormValue("email"),
-		Password: r.FormValue("password"),
-	}
-
-	u, ok := dbUsers[data.Username]
-	if !ok || u.Password != data.Password {
+	// Check if user exists and password is correct
+	user, ok := dbUsers[formUser.Username]
+	if !ok || user.Password != formUser.Password {
 		log.Warn("Invalid login attempt")
 		http.Error(w, "Invalid username or password", http.StatusUnauthorized)
 		return
 	}
 
-	cookie, err := r.Cookie("session")
-	if err == http.ErrNoCookie || dbSessions[cookie.Value] == "" {
-		id := uuid.NewV4()
-		cookie = &http.Cookie{
-			Name:     "session",
-			Value:    id.String(),
-			HttpOnly: true,
-		}
-		http.SetCookie(w, cookie)
-		dbSessions[id.String()] = data.Username
-		log.WithField("username", data.Username).Info("User logged in")
-		http.Redirect(w, r, "/bar", http.StatusSeeOther)
-		return
-	} else if err != nil {
-		log.WithError(err).Error("Unable to retrieve session cookie")
-		http.Error(w, "Unable to retrieve session cookie", http.StatusInternalServerError)
-		return
-	}
+	// Create session cookie
+	id := uuid.NewV4()
+	http.SetCookie(w, &http.Cookie{
+		Name:     "session",
+		Value:    id.String(),
+		HttpOnly: true,
+	})
+
+	// Store session in db
+	dbSessions[id.String()] = formUser.Username
+	log.WithField("username", formUser.Username).Info("User logged in")
+	// Redirect to index
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func signup(w http.ResponseWriter, r *http.Request) {
-	if alreadyLoggedIn(r) {
+	// If user is already logged in, redirect to index
+	if _, err := alreadyLoggedIn(r); err == nil {
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
 
-	if r.Method == http.MethodPost {
-		err := r.ParseForm()
+	// If request is not POST, render signup page
+	if r.Method != http.MethodPost {
+		err := tpl.ExecuteTemplate(w, "signup.gohtml", nil)
 		if err != nil {
-			log.WithError(err).Error("Unable to parse form")
-			http.Error(w, "Unable to parse form", http.StatusBadRequest)
-			return
+			log.WithError(err).Error("Unable to load template")
+			http.Error(w, "Unable to load template", http.StatusInternalServerError)
 		}
-	
-		data := User{
-			Username: r.FormValue("username"),
-			Email:    r.FormValue("email"),
-			Password: r.FormValue("password"),
-		}
-
-		if _, ok := dbUsers[data.Username]; ok {
-			log.Warn("Username already exists")
-			http.Error(w, "Username already exists", http.StatusConflict)
-			return
-		}
-	
-		dbUsers[data.Username] = data
-		log.WithFields(logrus.Fields{
-			"username": data.Username,
-			"email":    data.Email,
-		}).Info("New user signed up")
-		http.Redirect(w, r, "/login", http.StatusSeeOther)
 	}
 
-	err := tpl.ExecuteTemplate(w, "signup.gohtml", nil)
+	// If request is POST, parse form and create new user
+	formUser, err := parseForm(r)
 	if err != nil {
-		log.WithError(err).Error("Unable to load template")
-		http.Error(w, "Unable to load template", http.StatusInternalServerError)
+		http.Error(w, "Unable to parse form", http.StatusBadRequest)
+		return
 	}
+
+	// Check if username already exists
+	if _, ok := dbUsers[formUser.Username]; ok {
+		log.Warn("Username already exists")
+		http.Error(w, "Username already exists", http.StatusConflict)
+		return
+	}
+
+	// Create new user
+	dbUsers[formUser.Username] = formUser
+
+	log.WithFields(logrus.Fields{
+		"username": formUser.Username,
+		"email":    formUser.Email,
+	}).Info("New user signed up")
+
+	// Redirect to login
+	http.Redirect(w, r, "/login", http.StatusSeeOther)
+
 }
 
-func alreadyLoggedIn(r *http.Request) bool {
+// alreadyLoggedIn checks if the user is already logged in by checking the session cookie.
+// If the user is logged in, it returns the User struct. If the user is not logged in, it returns an error.
+func alreadyLoggedIn(r *http.Request) (User, error) {
+	// Check if session cookie exists
 	c, err := r.Cookie("session")
 	if err != nil {
-		return false
+		return User{}, fmt.Errorf("no session cookie")
 	}
 
+	// Check if session exists in db
 	username, ok := dbSessions[c.Value]
 	if !ok {
-		return false
+		return User{}, fmt.Errorf("invalid session cookie")
 	}
 
-	_, ok = dbUsers[username]
-	return ok
+	// Check if user exists in db
+	if user, ok := dbUsers[username]; ok {
+		return user, nil
+
+	}
+
+	// If user does not exist, return error
+	return User{}, fmt.Errorf("user not found")
 }
 
+// parseForm parses the form data from the HTTP request and returns a User struct.
+// It expects the form to contain "username", "email", and "password" fields.
+// If the form cannot be parsed, it returns an error.
+func parseForm(r *http.Request) (User, error) {
+	err := r.ParseForm()
+	if err != nil {
+		return User{}, err
+	}
+
+	// Check if form values are not empty
+	if r.FormValue("username") == "" || r.FormValue("email") == "" || r.FormValue("password") == "" {
+		return User{}, fmt.Errorf("all fields are required")
+	}
+
+	formUser := User{
+		Username: r.FormValue("username"),
+		Email:    r.FormValue("email"),
+		Password: r.FormValue("password"),
+	}
+
+	return formUser, nil
+}
